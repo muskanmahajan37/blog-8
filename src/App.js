e9810eff98336df37a1d355b6d99fa497a7b274e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-04-03 15:09:10 +0530

    adding code for dark theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./styles/app.css";
 
-function App() {
+// import GetJobs from "./services/GetJobs";
+
+function App(props) {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(getDefaultTheme());
+
+  function getDefaultTheme() {
+    const selectedTheme = JSON.parse(localStorage.getItem("dark"));
+    return selectedTheme || false;
+  }
+
+  const GetJobs = () => {
+    let url = "https://jobs.github.com/positions.json";
+    setLoading(true);
+    axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => {
+      console.log(response.data[0]);
+      setJobs(response.data);
+    });
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkTheme));
+    GetJobs();
+  }, [darkTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        className={darkTheme ? "dark-theme" : "light-theme"}
+        onClick={setDarkTheme(!darkTheme)}
+      ></div>
+      {loading && <h1>Loading...</h1>}
+      {/* {error && <h1>Error</h1>} */}
+      {jobs.map((job) => (
+        <h1 key={job.id}>{job.title}</h1>
+      ))}
     </div>
   );
 }
