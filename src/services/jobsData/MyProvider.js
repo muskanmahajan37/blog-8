1d20e8757eb491fc991449221123c4e82514fd41
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-04-03 23:52:45 +0530

    Added context api

--- src/services/jobsData/MyProvider.js
+++ src/services/jobsData/MyProvider.js
@@ -3,29 +3,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function MyProvider({ children }) {
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [jobs, setJobs] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
-  const GetJobs = () => {
-    // let url = "https://jobs.github.com/positions.json";
-    // setLoading(true);
-    // axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => {
-    //   console.log(response.data[0]);
-    //   setJobs(response.data);
-    // });
-    // setLoading(false);
-  };
+  // const GetJobs = () => {
+  //   // let url = "https://jobs.github.com/positions.json";
+  //   // setLoading(true);
+  //   // axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => {
+  //   //   console.log(response.data[0]);
+  //   //   setJobs(response.data);
+  //   // });
+  //   // setLoading(false);
+  // };
 
-  useEffect(() => {
-    // GetJobs();
-    let url = "https://jobs.github.com/positions.json";
-    setLoading(true);
-    axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => {
-      console.log(response.data[0]);
-      setJobs(response.data);
-    });
-    setLoading(false);
-  }, []);
+  // useEffect(() => {
+  //   // GetJobs();
+  //   let url = "https://jobs.github.com/positions.json";
+  //   setLoading(true);
+  //   axios.get(`https://cors-anywhere.herokuapp.com/${url}`).then((response) => {
+  //     console.log(response.data[0]);
+  //     setJobs(response.data);
+  //   });
+  //   setLoading(false);
+  // }, []);
+
+  const jobs = [1,2,3,4,5];
+  const loading = false;
 
   return (
     <MyContext.Provider
